44b2afc43d8dc9a54ddc0ae6b51c8061f4a567a3
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2025-08-09 11:47:57 +0200

    edit name

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { router } from './router'
-import './assets/main.css' // ou ton CSS global
+import './assets/main.css'
 
 const app = createApp(App)
 
